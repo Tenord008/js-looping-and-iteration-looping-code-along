b2a9504f80b4ccaea4c2f0c7e89410fd5d5b771b
Trent Renden <trent_renden@hotmail.com>
2021-04-30 11:50:57 +1000

    done wit assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+
+let names = ['Lisa', 'Kaitlin', 'Jan'];
+let message = `surprise`;
+
+function writeCards(names, message) {
+    for (let i = 0; i < names.length; i++) {
+      return [`Thank you, ${names[0]}, for the wonderful ${message} gift!`,
+      `Thank you, ${names[1]}, for the wonderful ${message} gift!`,
+      `Thank you, ${names[2]}, for the wonderful ${message} gift!`]
+      
+  }
+ 
+}
+
+function countDown(){
+
+  for (let countDown = 10; countDown >= 0; countDown--) {
+    console.log(countDown);
+  }
+
+}
+
+// let countDown =10
+
+// //let countDown = 10;
+// while (countDown > 0) {
+//     console.log(countDown);
+//   }
+     
+
+  
+
